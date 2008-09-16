9d446aadd4c55b2a1ac9ee42bef1940c1d9ca39c
Reinout van Rees <reinout@vanrees.org>
2008-09-16 10:02:37 +0000

    .1 release to get a fresh pypi upload
    svn path=/kss.plugin.timer/trunk/; revision=71904

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kss.plugin.timer',
-    version="0.1",
+    version="0.1.1",
     #description="",
     author="Guido Wesdorp and Zest software",
     author_email="info@zestsoftware.nl",
